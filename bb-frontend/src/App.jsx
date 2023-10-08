cc786caabde703e96decf3cee016b8920640f7ae
Ram Refai <ramrefai5@gmail.com>
2023-10-07 20:23:11 -0500

    Updated frontend

--- bb-frontend/src/App.jsx
+++ bb-frontend/src/App.jsx
@@ -4,76 +4,22 @@ import React, { useState, useEffect } from 'react';
 import './App.css'
 import AnimatedComponent from './components/AnimatedComponent';
 import TypeIt from "typeit-react";
-
+import { BrowserRouter, Router, Routes,Route, Link } from 'react-router-dom';
+import SignInButton from './components/SignInButton';
+import Home from './pages/Home';
+import Wallet from './pages/Wallet';
 
 function App() {
-  const [showTitle, setShowTitle] = useState(false);
-
-  useEffect(() => {
-    // Use setTimeout to delay showing the title
-    const timer = setTimeout(() => {
-      setShowTitle(true);
-    }, 1500);
-
-    // Clear the timer when the component unmounts
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []); // Empty dependency array to run the effect only once
-
-  const SuperStrong = ({ children }) => {
-    return <strong style={{ fontSize: "70px", className:"glitch" }}>{children}</strong>;
-  };
-
-  return (
-   
-    <div className="app-container">
-
-
-<div className="logo">
-        <a href="https://devpost.com/software/bytebucks" target="_blank">
-          <img src={'/BB_Logo.png'} className="logo" alt="ByteBucks Logo" />
-        </a>
-      </div>
-      
-      
-      <div className="content">
-
-        <div className="content-item">
-      <TypeIt>
-        <SuperStrong>Welcome to
-          ByteBucks.
-        </SuperStrong>
-      </TypeIt>
-      </div>
-
-      
-        
-    <AnimatedComponent>
-      <div className="content-item">
-    <div className="text-field">
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" />
-      </div>
-      </div>
-      <div className="content-item">
-      <div className="text-field">
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
-      </div>
-      </div>
-      <div className="content-item">
-      <button>
-       Sign in
-      </button>
-      </div>
-      </AnimatedComponent>
-     {/* <Login/> */}
-
-     </div>
-     
-    </div>
-
+  return(
+    <div>
+    <BrowserRouter>
+        <Routes>
+            <Route index element={<Home />} />
+            <Route path='/pages/Wallet' element={<Wallet />} />
+         
+        </Routes>
+    </BrowserRouter>
+</div>
   );
 }
 
---
+++ bb-frontend/src/pages/Home.jsx
@@ -0,0 +1,82 @@
+
+import React, { useState, useEffect } from 'react';
+import AnimatedComponent from '../components/AnimatedComponent';
+import TypeIt from "typeit-react";
+import { BrowserRouter as Router, Routes,Route, Link } from 'react-router-dom';
+import SignInButton from '../components/SignInButton';
+
+function Home() {
+  const [showTitle, setShowTitle] = useState(false);
+
+  useEffect(() => {
+    // Use setTimeout to delay showing the title
+    const timer = setTimeout(() => {
+      setShowTitle(true);
+    }, 1500);
+
+    // Clear the timer when the component unmounts
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []); // Empty dependency array to run the effect only once
+
+  const SuperStrong = ({ children }) => {
+    return <strong style={{ fontSize: "70px", className:"glitch" }}>{children}</strong>;
+  };
+
+  return (
+   
+
+    
+    <div className="app-container">
+
+<AnimatedComponent>
+<div className="logo">
+        <a href="https://devpost.com/software/bytebucks" target="_blank">
+          <img src={'/BB_Logo.png'} className="logo" alt="ByteBucks Logo" />
+        </a>
+      </div>
+      </AnimatedComponent>
+      
+      <div className="content">
+
+        <div className="content-item">
+      <TypeIt>
+        <SuperStrong>Welcome to
+          ByteBucks.
+        </SuperStrong>
+      </TypeIt>
+      </div>
+
+      
+        
+    <AnimatedComponent>
+      <div className="content-item">
+    <div className="text-field">
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
+      </div>
+      </div>
+      <div className="content-item">
+      <div className="text-field">
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+      </div>
+      </div>
+      <div className="content-item">
+      <SignInButton/>
+      </div>
+      </AnimatedComponent>
+     {/* <Login/> */}
+
+     </div>
+     
+    </div>
+
+
+
+
+
+  );
+}
+export default Home;
